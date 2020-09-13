9acc0f6c4cd5146157d5ec733af0827cd7bf9425
Guy Sheffer <guy@shapedo.com>
2020-09-13 20:12:22 +0300

    Sync process now runs the actual process, empty main window in place

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ import yaml
 TRAY_TOOLTIP = 'Name' 
 TRAY_ICON = 'icon.png'
 import os
+import schedule
+from dataclasses import dataclass
+import subprocess
+import time
+
+#TODO:
+# 1. Save logs so they can be shown in main screen
+# 2. main frame needs to show the backups and stuff from them
 
 
 debug = 'DEBUG' in os.environ and os.environ['DEBUG'] == "on"
@@ -19,6 +27,32 @@ def get_config():
 
 
 config = get_config()
+
+
+def _run_command(command, **kwargs):
+    if debug:
+        print(" ".join(command))
+    p = subprocess.Popen(command, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE, **kwargs)
+    stdout = p.stdout.read()
+    stderr = p.stderr.read()
+    try:
+        stdout = stdout.decode("utf-8")
+    except UnicodeDecodeError as e:
+        print("Error: can't decode stdout")
+        print(e)
+        print(stdout)
+        stdout = ""
+
+    try:
+        stderr = stderr.decode("utf-8")
+    except UnicodeDecodeError as e:
+        print("Error: can't decode stderr")
+        print(stderr)
+        print(e)
+        stderr = ""
+
+    return_value = [stdout, stderr]
+    return return_value
 
 
 def create_menu_item(menu, label, func):
@@ -76,10 +110,8 @@ class SettingsFrame(wx.Frame):
         self.Bind(wx.EVT_CLOSE, self.onClose)
         self.Centre()
         self.Show()
-        self.GetParent().sync_jobs.append(1)
         print(self.GetParent().sync_jobs)
         ## End Window init stuff ##
-
 
         self.panel = wx.Panel(self)
         self.button_ok = wx.Button(self.panel, label="OK")
@@ -109,6 +141,36 @@ class SettingsFrame(wx.Frame):
         # print(self)
 
 
+class MainFrame(wx.Frame):
+    """
+    Class used for creating frames other than the main one
+    """
+
+    def __init__(self, title, parent=None):
+        wx.Frame.__init__(self, parent=parent, title=title)
+        self.Bind(wx.EVT_CLOSE, self.onClose)
+        self.Centre()
+        self.Show()
+
+        ## End Window init stuff ##
+
+        print("aa")
+        print(len(self.GetParent().sync_jobs))
+
+        for job in list(self.GetParent().sync_jobs):
+            print(type(job))
+
+        # print(wx.geta.sync_jobs)
+
+    def onClose(self, event):
+        """"""
+        print("closing")
+        # TODO - also delete from memmory
+        #self.Hide()
+        self.Destroy()
+        # print(self)
+
+
 class TaskBarIcon(wx.adv.TaskBarIcon):
     def __init__(self, frame):
         self.frame = frame
@@ -118,7 +180,9 @@ class TaskBarIcon(wx.adv.TaskBarIcon):
 
     def CreatePopupMenu(self):
         menu = wx.Menu()
-        create_menu_item(menu, 'Settings', self.on_hello)
+        # TODO make settings menu
+        # create_menu_item(menu, 'Settings', self.on_hello)
+        create_menu_item(menu, 'Main', self.on_open_main)
         menu.AppendSeparator()
         create_menu_item(menu, 'Exit', self.on_exit)
         return menu
@@ -141,21 +205,126 @@ class TaskBarIcon(wx.adv.TaskBarIcon):
             # print(self.settings_frame.Show())
             print(dir(self.settings_frame))
 
+    def on_open_main(self, event):
+        print ('Opening Main')
+        if not hasattr(self, 'main_frame'):
+            self.main_frame = MainFrame("Main", self.frame)
+        else:
+            if not self.main_frame:
+                print("closed")
+                self.main_frame = MainFrame("Main", self.frame)
+            print(dir(self.main_frame))
+
     def on_exit(self, event):
         wx.CallAfter(self.Destroy)
         self.frame.Close()
 
 
+class SyncProcess(wx.Process):
+    def __init__(self, *args, **kw):
+        self.terminated = False
+        wx.Process.__init__(self, *args, **kw)
+
+    def OnTerminate(self, pid, status):
+        self.terminated = True
+        
+
+@dataclass
+class Backup:
+    name: str
+    source: str
+    dest: str
+    port: str
+    key: str
+    every: str
+    time: str
+    window: wx.Frame
+
+    def __post_init__(self):
+        self.process_object = None
+        self.pid = None
+        print("Starting: " + str(self.name))
+        if self.every == "daily":
+            # schedule.every().seconds.do(
+            #     lambda: self.run_backup())
+            schedule.every().day.at(self.time).do(lambda: self.run_backup())
+
+    def run_backup(self):
+        print("hello!!!!!!!!!!!!!!")
+        config = get_config()
+
+        self.process_object = SyncProcess(self.window)
+        self.process_object.Redirect()
+        cmd = [config["main"]["bin"],
+               "-v6",
+               " --remote-schema 'ssh -p " + str(self.port) + " -i " + self.key + " %s rdiff-backup --server'",
+               "--",
+               self.source, self.dest]
+        print("running: " + str(" ".join(cmd)))
+        self.pid = wx.Execute(" ".join(cmd), wx.EXEC_ASYNC, callback=self.process_object)
+        print("pid: " + str(self.pid))
+        time.sleep(1)
+        stream = self.process_object.GetInputStream()
+
+        if stream.CanRead():
+            text = stream.read()
+            print(text)
+            wx.LogMessage(text)
+
+        print("Finish reading")
+
+        return
+
+
 class MainInvisibleWindow(wx.Frame):
     def __init__(self):
         wx.Frame.__init__(self, parent=None)
+
         self.sync_jobs = []
+        for backup in config["backups"]:
+            backup_class = Backup(**backup, window=self)
+            self.sync_jobs.append(backup_class)
+        print(len(self.sync_jobs))
+
+        self.Bind(wx.EVT_IDLE, self.OnIdle)
+
+        self.on_timer()
+
+    def on_timer(self):
+        # wx.CallLater(1000 * 60, self.on_timer)
+        wx.CallLater(1000, self.on_timer)
+        schedule.run_pending()
+        # print("bu")
+
+    def OnIdle(self, evt):
+        # print("idle")
+        if self.sync_jobs is not None:
+            for sync_job in self.sync_jobs:
+                if sync_job.process_object is not None:
+                    if sync_job.process_object.terminated:
+                        sync_job.process_object = None
+                    else:
+                        try:
+                            stream = sync_job.process_object.GetInputStream()
+
+                            if stream.CanRead():
+                                text = stream.read()
+
+                                print(text.decode())
+                        except RuntimeError as e:
+                            print(e)
+                            # import code;
+                            # code.interact(local=dict(globals(), **locals()))
+
+                # print("Done idle")
 
 
 class App(wx.App):
 
     def OnInit(self):
-        print("yay")
+        wx.Log.SetActiveTarget(wx.LogStderr())
+
+        print("Staring App OnInit")
         frame = MainInvisibleWindow()
         self.SetTopWindow(frame)
         TaskBarIcon(frame)
