c49a2050ce864fd26b8c2802bc86a7192aea7530
Guy Sheffer <guysoft@gmail.com>
2020-11-21 21:40:14 +0200

    Add wxpython to imports so pyinstaller will find it

--- src/backupfriend-client.py
+++ src/backupfriend-client.py
@@ -1,5 +1,5 @@
-from backupfriend.main import main
 import wx
+from backupfriend.main import main
 # Needed so pyinstaller will detect it needs this module
 import backupfriend.sub
 if __name__ == "__main__":
