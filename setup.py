50dff69ad77cb2eaca5f6fe07ba3c15861caaaca
Guy Sheffer <guysoft@gmail.com>
2021-07-01 13:22:05 +0300

    Remove rdiff-backup for mac

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ install_requires=[
         "wxPython", "PyYAML", "schedule", 'dataclasses;python_version<"3.7"', "appdirs", "rdiff-backup", "cryptography", "pypubsub"]
 
 if sys.platform == "darwin":
-    install_requires = [        "wxPython", "PyYAML", "schedule", 'dataclasses;python_version<"3.7"', "appdirs", "rdiff-backup", "cryptography", "pypubsub"]
+    install_requires = [        "wxPython", "PyYAML", "schedule", 'dataclasses;python_version<"3.7"', "appdirs", "cryptography", "pypubsub"]
 
 
 setuptools.setup(
