41156d603ba839415f6969591e0d2a6798e5b29e
Guy Sheffer <guysoft@gmail.com>
2021-07-04 18:23:57 +0300

    Try to add more libs

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ P2APP_OPTIONS = {
     'argv_emulation': True,
     'site_packages': True,
     #'iconfile': 'appicon.icns',
-    'packages': ["schedule", "wx._xml", 
+    'packages': ["schedule", "wx._xml", "wx.xml", "wx.lib", "wx.lib.ticker", "wx.xrc",
                  "appdirs", "cryptography"],
     'plist': {
         'CFBundleName': 'BackupFriend',
