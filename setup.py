6e8f2761ac2688aa47fc12a486024e0c519116ee
Guy Sheffer <guysoft@gmail.com>
2021-07-04 16:21:35 +0300

    Try to add etgtools for wx._xml

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ P2APP_OPTIONS = {
     'argv_emulation': True,
     'site_packages': True,
     #'iconfile': 'appicon.icns',
-    'packages': ["schedule", "wx._xml",
+    'packages': ["schedule", "wx._xml", "etgtools",
                  "appdirs", "cryptography"],
     'plist': {
         'CFBundleName': 'BackupFriend',
